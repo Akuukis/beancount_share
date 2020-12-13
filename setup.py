980a23f6670d85b1dc7643f2fec034ce6074d45f
Akuukis <akuukis@kalvis.lv>
2020-12-13 11:46:29 +0200

    chore: publish 0.1.2

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='beancount_share',
-    version='0.1.0',
+    version='0.1.2',
     description='Plugin for Beancount to share expenses.',
 
     long_description=long_description,
     long_description_content_type='text/markdown',
 
-    author='Kalvis \'Akuukis\' Kalnins',
+    author='Akuukis',
     author_email='akuukis@kalvis.lv',
     download_url='https://pypi.python.org/pypi/beancount_share',
     license='GNU AGPLv3',
